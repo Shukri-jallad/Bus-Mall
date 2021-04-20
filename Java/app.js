cd7d4e1a00d8dedd0cb890f5d17d5306c90bf671
Shukri-jallad <shukri.jallad@yahoo.com>
2021-04-20 23:09:52 +0300

    lab12

--- Java/app.js
+++ Java/app.js
@@ -3,7 +3,6 @@
 let firstImageElement = document.getElementById('first-image');
 let secondImageElement = document.getElementById('second-image');
 let thirdImageElement = document.getElementById('third-image');
-
 let button = document.getElementById('button');
 
 
@@ -12,6 +11,10 @@ let maxAttempts = 10;
 let firstIndex; 
 let secondIndex;
 let thirdIndex;
+let previouslyShown = [];
+let arrOfnames = [];
+let arrOfVotes = [];
+let arrOfShown = [];
 
 function BusMall(name,source){
   this.name= name;
@@ -19,6 +22,7 @@ function BusMall(name,source){
   this.votes = 0;
   this.dispCh=0;
   BusMall.allImages.push(this);
+  arrOfnames.push(this.name);
 }
 
 
@@ -55,11 +59,13 @@ function renderThreeImages(){
   secondIndex = genrateRandomIndex();
   thirdIndex = genrateRandomIndex();  
  
-  while(firstIndex === secondIndex || firstIndex === thirdIndex || secondIndex === thirdIndex){
+  while(firstIndex === secondIndex || firstIndex === thirdIndex || secondIndex === thirdIndex || previouslyShown.includes(firstIndex) || previouslyShown.includes(secondIndex) || previouslyShown.includes(thirdIndex)){
     firstIndex = genrateRandomIndex();
     secondIndex = genrateRandomIndex();
     thirdIndex = genrateRandomIndex();
   }
+
+  previouslyShown = [firstIndex,secondIndex,thirdIndex];
  
   firstImageElement.src =  BusMall.allImages[firstIndex].source;
   secondImageElement.src = BusMall.allImages[secondIndex].source;
@@ -103,12 +109,59 @@ button.addEventListener('click',function(){
     for(let i = 0 ; i < BusMall.allImages.length;i++){
       let li = document.createElement('li');
       ul.appendChild(li);
+      arrOfVotes.push(BusMall.allImages[i].votes);
+      arrOfShown.push(BusMall.allImages[i].dispCh);
       li.textContent = `${BusMall.allImages[i].name} it has ${BusMall.allImages[i].votes} Votes, was displayed ${BusMall.allImages[i].dispCh} times`;
     }
+    chart();
 
 });
 
 function genrateRandomIndex(){
    return Math.floor(Math.random() * BusMall.allImages.length); 
                   
+}
+function chart(){
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: arrOfnames,
+        datasets: [{
+            label: 'Votes per image',
+            data: arrOfVotes,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },{
+            label:'Times displayed',
+            data: arrOfShown,
+            backgroundColor:[
+              "rgb(172,192,92)"
+            ],
+            borderWidth: 1
+          }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 }
